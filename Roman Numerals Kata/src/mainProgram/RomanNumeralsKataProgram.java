a623e18b13255353a7731df0870a22c4b4350b68
DavidLangen <pbg2h15aln@pb.bib.de>
2017-09-27 17:54:39 +0200

    Implementierung der Anforderungen für den Encoder

--- Roman Numerals Kata/src/mainProgram/RomanNumeralsKataProgram.java
+++ Roman Numerals Kata/src/mainProgram/RomanNumeralsKataProgram.java
@@ -8,7 +8,15 @@ public class RomanNumeralsKataProgram {
 	}
 	
 	public static String romanNumeralsEncoder(int n){
-		return "";
+		if(n<=0){
+			return "";
+		}
+		String ergebnis= "";
+		for (int i = 0; i < n; i++) {
+			ergebnis += "I";
+		}
+		
+		return ergebnis;
 	}
 	
 	public static int romanNumeralsDecoder(String s){
