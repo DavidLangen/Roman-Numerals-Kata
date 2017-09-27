7c308f97df69231a4b4c02b64425ed0fed3ca681
DavidLangen <pbg2h15aln@pb.bib.de>
2017-09-28 00:16:29 +0200

    Erster Entwurf für den Decoder

--- Roman Numerals Kata/src/mainProgram/RomanNumeralsKataProgram.java
+++ Roman Numerals Kata/src/mainProgram/RomanNumeralsKataProgram.java
@@ -31,6 +31,32 @@ public class RomanNumeralsKataProgram {
 	}
 	
 	public static int romanNumeralsDecoder(String s){
-		return 0;
+		if(s.matches("[MCDXLVI]+") && !s.isEmpty()){
+			
+			int ergebnis = 0;
+			String restString = s;
+			
+			if(s.equals("IV")){
+				return 4;
+			}
+			
+			String[] r_zeichen = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+			int[] r_werte = {1000,900,500,400,100,90,50,40,10,9,5,4,1}; 
+			
+			for (int i = r_zeichen.length-1; i > 0  && !restString.isEmpty(); i--) {
+				String letztesZ = restString.substring(restString.length()-1, restString.length());
+				
+				if(r_zeichen[i].equals(letztesZ)){
+					ergebnis+=r_werte[i];
+					int index = restString.lastIndexOf(r_zeichen[i]);
+					restString = restString.substring(0,index);
+					i=r_zeichen.length;
+				}else{
+					continue;
+				}
+			}
+			return ergebnis;
+		}
+		return -1;
 	}
 }
