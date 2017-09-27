3f15ec9d4587c0f4d96e8bdaef81d9aaca653087
DavidLangen <pbg2h15aln@pb.bib.de>
2017-09-27 17:59:22 +0200

    Hinzufügen von Test für 4 und 5

--- Roman Numerals Kata/src/jUnitTests/RomanNumeralsKataTest.java
+++ Roman Numerals Kata/src/jUnitTests/RomanNumeralsKataTest.java
@@ -16,5 +16,17 @@ public class RomanNumeralsKataTest {
 	@Test
 	public void testNegativEncoder(){
 		assertEquals("", RomanNumeralsKataProgram.romanNumeralsEncoder(-1));
+		assertEquals("", RomanNumeralsKataProgram.romanNumeralsEncoder(0));
 	}
+	
+	@Test
+	public void testVier(){
+		assertEquals("IV", RomanNumeralsKataProgram.romanNumeralsEncoder(4));
+	}
+	
+	@Test
+	public void testFuenf(){
+		assertEquals("V", RomanNumeralsKataProgram.romanNumeralsEncoder(4));
+	}
+	
 }
